afd6df5a08439c0aed7271ac9aaeb5864f7a94f2
Omar <stellaar.oz@gmail.com>
2016-11-23 22:17:46 -0300

    changed the random number function

--- adivina.js
+++ adivina.js
@@ -8,15 +8,15 @@ var checkDif = function(n1, n2) {
   }
   document.getElementById('debug0').innerHTML = diff;
   switch (true) {
-    case diff <= 3:
+    case diff <= 3 && diff >= 1:
       document.getElementById('output').innerHTML = 'Caliente caliente!... ' +
       'casi le atinas!';
       break;
-    case diff <= 6:
+    case diff <= 6 && diff >= 4:
       document.getElementById('output').innerHTML = 'Tibio tibio... ' +
-      'estuviste por ahi.';
+      'uff, muy lejos!';
       break;
-    case diff <= 10:
+    case diff <= 10 && diff >= 7:
       document.getElementById('output').innerHTML = 'Frio ... Ni te acercaste.';
       break;
     default:
@@ -26,7 +26,11 @@ var checkDif = function(n1, n2) {
 
 // Activado por el boton del paso 1
 var magicNum = function() {
-  num = Math.floor((Math.random() * 10));
+  //Opcion 1 para crear el numero aleatorio
+  //num = Math.floor((Math.random() * 10));
+
+  //Opcion 2 para crear el numero aleatorio
+  num = getRandomInt(1, 11);
   document.getElementById('debug2').innerHTML = num;
 };
 
@@ -45,8 +49,8 @@ var revisarNum = function() {
   }
   else {
     if (userAnswer == num) {
-      document.getElementById('output').innerHTML = 'Bien! Lo adivinaste! ' +
-      'el numero es: ' + num;
+      document.getElementById('output').innerHTML = 'Felicidades! Lo adivinaste! ' +
+      'el numero es: ' + num + '. ' + 'Te ganaste una pizza. Va en camino a tu casa!';
       document.getElementById('final').innerHTML = 'Lo has conseguido!';
     }
     else if (userAnswer < 1 | userAnswer > 10) {
@@ -74,3 +78,11 @@ var playGate = function() {
     revisarNum();
   }
 };
+
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
